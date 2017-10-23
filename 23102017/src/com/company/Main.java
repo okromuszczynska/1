15213392d2e0fa1dd8eeb2e1f2666ebc17fc8d92
Unknown <okromuszczynska@gmail.com>
2017-10-23 19:17:02 +0200

    q
    q

--- 23102017/src/com/company/Main.java
+++ 23102017/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        User u1 = new User("u1@mail");
+        User u2 = new User("u1@mail");
+
+        System.out.println(u1.equals(u2));
+
+
+
     }
 }
---
+++ 23102017/src/com/company/User.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class User {
+
+    String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return email != null ? email.hashCode() : 0;
+    }
+}
